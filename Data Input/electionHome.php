7fbb416d34383278e6947684bf2b1c48c0d8e6e7
Anouk Stein <anouk.stein@gmail.com>
2015-06-07 13:51:03 -0400

    Data Input UI
    UI for entering Princeton Election Data to MySQL and downloading result
    as csv or json.

---
+++ Data Input/electionHome.php
@@ -0,0 +1,50 @@
+<!--Copyright ©2015 Anouk Stein, MD-->
+<html>
+ <head>
+<link rel="stylesheet" type="text/css" href="pr.MyStylePlain.css" />
+  <title>Election</title>
+ </head>
+ <body style="text-align: center; background-color: #F3E2A9">
+    <br><br>
+    <h1>Choose Election District and Machine</h1>
+ <?php
+include "SaveElection.php";
+connect();
+
+
+echo "Select the district: ";
+echo "<form action = './electionSpecificInput.php' name='choices' method ='post'> ";
+echo "<select name='district'>";
+
+$number_of_districts = 22;
+for ($i=1; $i<=$number_of_districts; $i++){
+echo '<OPTION VALUE="' . $i . '">' . $i ;
+}
+
+echo "</select><br><br>Select the machine: ";
+echo "<select name='machine'>";
+
+
+$maximum_number_machines = 3;
+for ($i=1; $i<=$maximum_number_machines; $i++){
+echo '<OPTION VALUE="' . $i . '">' . $i ;
+}
+?>
+</select><br><p><input type=submit></p>
+ <br><br><br><br><h2>Districts entered:</h2>
+
+<?php
+//data entered
+for ($d=1; $d<= $number_of_districts; $d++){
+    for ($m=1; $m<=$maximum_number_machines; $m++){
+        if (dataEntered($d,$m) == true){
+            echo"District $d, Machine $m <br> ";
+        }
+    }
+}
+mysql_close();
+?>
+
+</form>
+ </body>
+</html>
